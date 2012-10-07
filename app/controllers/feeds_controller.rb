40169fab9d2339ebebef72831846c219d4cae91a
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-06 21:56:50 -0400

    update feeds#fetch

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,20 +7,31 @@ class FeedsController < ApplicationController
   def fetch
     feedHash = {'cnn' => "http://rss.cnn.com/rss/cnn_mostpopular.rss",
       'espn' => "http://sports.espn.go.com/espn/rss/news"}
-    feed = Feedzirra::Feed.fetch_and_parse(feedHash[params[:feed]])
+    
+    feeds = []
+     
+    params[:feed].split(",").each  do |f|
+      feeds.push(Feedzirra::Feed.fetch_and_parse(feedHash[f]))
+    end  
+      
     items = []
     
-    feed.entries.each do |entry|
-      items.push({"title" => entry.title,
-                  "summary" => entry.summary,
-                  "content" => entry.content,
-                  "url" => entry.url,
-                  "published" => entry.published
-                   })
+    
+    
+    feeds.each do |feed|
+      feed.entries.each do |entry|
+        items.push({"title" => entry.title,
+                    "summary" => entry.summary,
+                    "content" => entry.content,
+                    "url" => entry.url,
+                    "published" => entry.published,
+                    "feed_title" => feed.title
+                    })
+      end  
     end  
     
       begin
-        render :json => { 'title' => feed.title, 'items' => items }
+        render :json => { 'items' => items }
       rescue
         render :json => { 'title' => "krapp!!", 'items' => [] }
       end    
