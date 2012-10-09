a0a3a6c5da587390bb0036ee089551f4cea2ab32
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-09 14:38:39 -0400

    fix datatypes

--- db/migrate/20121006185002_create_bathrooms.rb
+++ db/migrate/20121006185002_create_bathrooms.rb
@@ -6,8 +6,8 @@ class CreateBathrooms < ActiveRecord::Migration
       t.string :city
       t.string :state
       t.string :zip
-      t.string :lat
-      t.string :lng
+      t.float :lat, :limit => 53
+      t.float :lng, :limit => 53
       t.boolean :isHandicapAccessible
       t.boolean :isPublic
       t.boolean :isFamily
