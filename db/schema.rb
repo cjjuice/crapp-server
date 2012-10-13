e8a36484555a073a4ee4589957e6a2e4ef71e801
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-13 14:59:35 -0400

    switch branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121007134820) do
+ActiveRecord::Schema.define(:version => 20121006185011) do
 
   create_table "bathrooms", :force => true do |t|
     t.string   "name"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(:version => 20121007134820) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.float    "lat", :limit => 53
-    t.float    "lng", :limit => 53               
+    t.float    "lat"
+    t.float    "lng"
     t.boolean  "isHandicapAccessible"
     t.boolean  "isPublic"
     t.boolean  "isFamily"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(:version => 20121007134820) do
     t.boolean  "hasSignage"
     t.boolean  "hasWifi"
     t.integer  "bathroomtype_id"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
   end
 
   add_index "bathrooms", ["bathroomtype_id"], :name => "index_bathrooms_on_bathroomtype_id"
