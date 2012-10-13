3f544bc426b5fedd477b314b28a527e2ee7790b8
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-13 15:34:19 -0400

    secure api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :restrict_access
+
+  private
+
+  def restrict_access
+    api_key = ApiKey.find_by_access_token(params[:access_token])
+    head :unauthorized unless api_key
+  end
 end
